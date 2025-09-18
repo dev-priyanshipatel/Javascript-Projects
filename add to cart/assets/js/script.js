08dc9c74097cedaafaf045ebcd5c0db184fc4b4d
dev-priyanshipatel <priyanshipatel8304@gmail.com>
2025-09-18 10:51:17 +0530

    added card for student managment

---
+++ add to cart/assets/js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  slidesPerView: 1,
+  // spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+});
